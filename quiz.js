410d3b1a6d795ca3f4f67f2403fc52fdaa3cb038
Favour Oluchi Boi <58810681+Orluchy@users.noreply.github.com>
2020-05-07 16:55:24 +0100

    Question data edited

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const questionCounterText = document.getElementById("questionCounter");
 const scoreText = document.getElementById("score");
-const nextButton = document.getElementById("next-btn");
+const nextButton = document.getElementById("nextbutn");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -10,61 +10,58 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-let quizOption1 = document.getElementById("option-1");
-let quizOption2 = document.getElementById("option-2");
-let quizOption3 = document.getElementById("option-3");
-let quizOption4 = document.getElementById("option-4");
+let quizChoice1 = document.getElementById("choice-1");
+let quizChoice2 = document.getElementById("choice-2");
+let quizChoice3 = document.getElementById("choice-3");
+let quizChoice4 = document.getElementById("choice-4");
 
 
 let questions = [
   {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
+    question: "Relays are generally divided into _______ & ______ Relays?",
+    choice1: "Manual & Automated",
+    choice2: "Digital & Manual",
+    choice3: "Electromechanical  & Solid-state",
+    choice4: "Simple & Complex",
     answer: 3
   },
   {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
+    question: "How many characters can a QR code hold?",
+    choice1: "1024",
+    choice2: "4296",
+    choice3: "5022",
+    choice4: "680",
+    answer: 2
+  },
+  {
+    question: "How much was recently recovered from Abacha's loot from the US,
+    choice1: "$281 million",
+    choice2: "$218 million",
+    choice3: "$321 million",
+    choice4: "$312 million ",
     answer: 4
   },
   {
-    question:
-      "An ADT is defined to be a mathematical model of a user-defined type along with the collection of all ____________ operations on that model",
-    option1: "Primitive",
-    option2: "Assignment",
-    option3: "Cardinality",
-    option4: "Structured",
+    question: "In chess, how many pieces does each player begin with?",
+    choice1: "16",
+    choice2: "32",
+    choice3: "10",
+    choice4: "22",
     answer: 1,
   },
   {
-    question:
-      "Which of the following abstract data types can be used to represent a many to many relation ?",
-    option1: "Tree",
-    option2: "Plex",
-    option3: "Graph",
-    option4: "Both (b) and (c)",
+    question: "_________ is the first colonizer of a bare rock",
+    choice1: "sundew",
+    choice2: "liverwort",
+    choice3: "lichen",
+    choice4: "mosses",
     answer: 4,
   },
 ];
 
 //CONSTANTS
-const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 5;
+const CORRECT_Bonus = 10;
+const MAX_Questions = 5;
 
 startGame = () => {
   questionCounter = 0;
@@ -75,11 +72,11 @@ startGame = () => {
 
 getNewQuestion = () => {
   if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    //go to the end page
+    //go to the end page instead
     return window.location.assign("end.html");
   }
   questionCounter++;
-  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+  questionCounterText.innerText = `${questionCounter}/${MAX_Questions}`;
 
   const questionIndex = Math.floor(Math.random() * availableQuesions.length);
   currentQuestion = availableQuesions[questionIndex];
@@ -106,7 +103,7 @@ choices.forEach(choice => {
       selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
     if (classToApply === "correct") {
-      incrementScore(CORRECT_BONUS);
+      incrementScore(CORRECT_Bonus);
     }
 
     selectedChoice.parentElement.classList.add(classToApply);
@@ -114,8 +111,12 @@ choices.forEach(choice => {
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToApply);
       getNewQuestion();
-    }, 1000);
+    }, 550);
   });
+});
+
+nextButton.addEventListener("click", (event) => {
+  getNewQuestion();
 });
 
 incrementScore = num => {
